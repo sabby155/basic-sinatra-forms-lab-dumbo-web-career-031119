7bacc8ab97a659169e6748192711fe19843a68a3
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-02 13:33:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end  
+  
+  post '/team' do 
+    @name = params[:name]
+    @coach = params[:coach]
+    @point_gu = params[:pg]
+    @shooting_gu = params[:sg]
+    @power_fo = params[:pf]
+    @small_fo = params[:sf]
+    @c = params[:c]
+    erb :team
+  end  
+  
+  
 
 end
